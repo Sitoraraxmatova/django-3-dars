b73b2323446716a15827fe30495950f25186bba4
muslimbek77 <muslimjonbekali@mail.ru>
2024-03-28 21:09:36 +0500

    add .gitignore file

--- myapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse #new
-# Create your views here.
-
-def main(request,name):
-    return HttpResponse(f"Salom {name}")
-
-# def name(request);
-#     print
