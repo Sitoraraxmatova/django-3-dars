9634a602e7b506300fb7e2622cd2474580dc9446
muslimbek77 <muslimjonbekali@mail.ru>
2024-03-30 19:43:17 +0500

    change admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display=["title","create_data"]
 
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    is_active = models.BooleanField(default=False)
+    image = models.ImageField(upload_to="Article/image")
+    create_data = models.DateTimeField(auto_now=True)
+    #4ta maydon qo'shish
+    def __str__(self) -> str:
+        return f"{self.title}"
